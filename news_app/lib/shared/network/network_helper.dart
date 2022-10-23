f4fb61f7d65c5d6e56f3880ab56a179e116f9d66
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-23 22:30:48 +0200

    Added dio and new methods to fetch data

--- news_app/lib/shared/network/network_helper.dart
+++ news_app/lib/shared/network/network_helper.dart
@@ -1,10 +1,18 @@
+import 'package:dio/dio.dart';
+
 class NetworkHelper {
-  static Dio dio;
+  static late Dio dio;
 
   static initData(){
-    dio = Dio(BaseOption(
-      url: "https://newsapi.org/",
+    dio = Dio(BaseOptions(
+      baseUrl : "https://newsapi.org/",
       receiveDataWhenStatusError: true
     ));
   }
+
+  static Future<Response> getData({required String url, required Map<String,dynamic> query}) async {
+   return await dio.get(url, queryParameters:query);
+  }
+
+
 }
