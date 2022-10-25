9940a65ed47b7137c96b99f3bf6d3d3c64329c1a
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-25 15:58:23 +0200

    Added three classes for business

--- news_app/lib/layout/news_app/cubit/news_state.dart
+++ news_app/lib/layout/news_app/cubit/news_state.dart
@@ -6,3 +6,12 @@ abstract class NewsState {}
 class NewsInitial extends NewsState {}
 
 class NewsBottomNavState extends NewsState {}
+
+class NewsGetBusinessLoadingState extends NewsState {}
+
+class NewsGetBusinessSucsessState extends NewsState {}
+
+class NewsGetBusinessErorrState extends NewsState {
+   final String erorr;
+   NewsGetBusinessErorrState(this.erorr);
+}
