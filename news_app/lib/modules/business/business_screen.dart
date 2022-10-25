d27dc1f4b93a2b903cda7acf648d638b0e1b41bf
3li-3bdullah <PROGRMMER7@gmail.com>
2022-10-25 16:37:44 +0200

    Used the custom newsbody

--- news_app/lib/modules/business/business_screen.dart
+++ news_app/lib/modules/business/business_screen.dart
@@ -2,25 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:news_app/layout/news_app/cubit/news_cubit.dart';
 import 'package:news_app/shared/components/custom_card.dart';
+import 'package:news_app/shared/components/news_body.dart';
 
 class BusinessScreen extends StatelessWidget {
   const BusinessScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<NewsCubit,NewsState>(
-      listener: (context,state){},
-      builder: (context,state){
-        List<Map<String, dynamic>> list = NewsCubit.get(context).business;
-       return state is NewsGetBusinessLoadingState ? 
-       const Center(child: CircularProgressIndicator(),) :
-       ListView.separated(
-        physics: const BouncingScrollPhysics(),
-        itemCount:  10,
-        separatorBuilder: (context,inex) => const Divider(height: 1,),
-        itemBuilder: (context,index) => CustomCard(dataList: list[index]),
-        ) ;
-      },
-      );
+    return BlocConsumer<NewsCubit, NewsState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          List<Map<String, dynamic>> list = NewsCubit.get(context).business;
+          return NewsBody(list: list);
+        });
   }
 }
