425dc09da30ee7858a69bb2549996d18f954a311
3li-3bdullah <PROGRMMER7@gmail.com>
2022-11-09 02:05:23 +0200

    Added blocProvider and mehods

--- news_app/lib/shared/cubit/cubit/app_cubit.dart
+++ news_app/lib/shared/cubit/cubit/app_cubit.dart
@@ -1,8 +1,18 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
 
 part 'app_state.dart';
 
 class AppCubit extends Cubit<AppState> {
   AppCubit() : super(AppInitial());
+
+
+  static AppCubit get (context) => BlocProvider.of(context);
+
+  bool isDark = false;
+ void changeMode(){
+  isDark = !isDark;
+  emit(AppChangeThemeModeState());
+ }
 }
